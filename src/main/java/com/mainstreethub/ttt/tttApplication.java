f0ffd8e6c8cd1c5ad6c08665037bb97d9f86ffef
Taylor Bigham <mtbigham@ufl.edu>
2015-09-07 16:14:52 -0400

    Initial Application commit

---
+++ src/main/java/com/mainstreethub/ttt/tttApplication.java
@@ -0,0 +1,7 @@
+package com.mainstreethub.ttt;
+
+public class tttApplication extends Application<tttConfig>{
+	public static void main(String[] args) throws Exception{
+		new tttApplication().run(args);
+	}
+}
